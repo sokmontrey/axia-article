d964ca08a5af252b4f14309deb933657a749dd2d
Sokmontrey <sythatsokmontrey@gmail.com>
2024-02-22 21:07:38 -0500

    new files structure

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,36 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import tailwind from "@astrojs/tailwind";
+import rehypePrettyCode from "rehype-pretty-code";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import svelte from "@astrojs/svelte";
+const pretty_opts = {
+  theme: "poimandres",
+  onVisitLine(node) {
+    // Prevent lines from collapsing in `display: grid` mode, and
+    // allow empty lines to be copy/pasted
+    if (node.children.length === 0) {
+      node.children = [{
+        type: "text",
+        value: " "
+      }];
+    }
+  },
+  onVisitHighlightedLine(node) {
+    // Adding a class to the highlighted line
+    if (!node.properties.className) node.properties.className = [];
+    node.properties.className.push("highlighted");
+  }
+};
+const katex_opts = {};
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()]
+  integrations: [mdx(), tailwind(), svelte()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [[rehypePrettyCode, pretty_opts], [rehypeKatex, katex_opts]],
+    remarkPlugins: [remarkMath]
+  }
 });
